779cf3879a2ebfc5194fcd02146babae0d88ab39
Amanda Lee <amanda@massiveinfinity.com>
2020-04-14 10:55:13 +0800

    added back option 1

--- run_script.sh
+++ run_script.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 #calling the other script within the current directory
+#option 1
+#./hello_world.sh
+#./greet_user.sh bob
+#./add_number.sh 1 2
+
+
+#option 2
 set -e
 ./hello_world.sh
 ./greet_user.sh
 ./add_number.sh 1 2
 
 #echo    "hello world"
+
